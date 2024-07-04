55336baaf1882fc43c65a8affb659e828b3677f0
Antonin <antonin.chaudiere@hotmail.fr>
2024-07-04 13:20:08 +0200

    Init contact manager project

---
+++ src/main/java/com/api_contact_manager/ApiContactManagerApplication.java
@@ -0,0 +1,13 @@
+package com.api_contact_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContactManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiContactManagerApplication.class, args);
+    }
+
+}
