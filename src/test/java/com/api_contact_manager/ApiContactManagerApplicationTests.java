b231a9be4aad5bde8a5cd395a5d2ff05dd596d96
Toniin <antonin.chaudiere@hotmail.fr>
2024-08-30 13:29:05 +0200

    Add hibernate settings in config app

--- src/test/java/com/api_contact_manager/ApiContactManagerApplicationTests.java
+++ src/test/java/com/api_contact_manager/ApiContactManagerApplicationTests.java
@@ -3,7 +3,7 @@ package com.api_contact_manager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ApiContactManagerApplicationTests {
 
     @Test
