7d5e9bb9f3bea813881c64fab913afa0ca1afdd3
Toniin <antonin.chaudiere@hotmail.fr>
2024-08-30 15:33:53 +0200

    Add maven surefire plugin in pom.xml

--- src/test/java/com/api_contact_manager/ApiContactManagerApplicationTests.java
+++ src/test/java/com/api_contact_manager/ApiContactManagerApplicationTests.java
@@ -1,11 +1,11 @@
-package com.api_contact_manager;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiContactManagerApplicationTests {
-
-    @Test
-    void contextLoads() {}
-}
+//package com.api_contact_manager;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApiContactManagerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {}
+//}
